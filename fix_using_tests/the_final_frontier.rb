ea37176fc87a90fbaa26799d9d23eebbb5735acd
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-23 03:27:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,7 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+ (rand(100000) + 400000) / 10.0
 end
 
 
@@ -12,4 +12,5 @@ end
 def engage
   puts state_log(date)
   date = generate_star_date
+  binding 
 end
